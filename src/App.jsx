46607589f4e1b45d4974c54591bc22797efc766c
connorhills <connorhills2023@gmail.com>
2025-08-08 12:15:46 -0400

    Added Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Home } from "@/pages/Home";
 import { NotFound } from "@/pages/NotFound";
 import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
-  return ( 
+  return (
     <>
-    <Toaster />
+      <Toaster />
       <BrowserRouter>
         <Routes>
-          <Route index element={<Home />}/>
-          <Route path="*" element={<NotFound />}/>
+          <Route index element={<Home />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
+      <Analytics />
     </>
   );
 }
